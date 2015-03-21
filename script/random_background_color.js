7a76b1c4e9fdc099c051b8378369b5c8ab569e4d
aytymoshenko <aytymoshenko@gmail.com>
2015-03-21 19:05:19 +0200

    added what color is now

--- script/random_background_color.js
+++ script/random_background_color.js
@@ -1,4 +1,5 @@
 function random_background_color () {
 	var random_color = "#" + Math.floor(Math.random() * 16777215).toString(16);
 	document.getElementById("body").style.background = random_color;
+	document.getElementById("color").innerHTML = random_color.toUpperCase();
 }
